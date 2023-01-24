6317e87d768bdcae1f06f5da6b0d698544d2fc48
Samuel-lgd <lagarde.samuel@gmail.com>
2023-01-24 14:48:15 +0100

    mouvement de base

--- README.md
+++ README.md
@@ -5,3 +5,28 @@
 Possibilité de jouer en solo contre l'ordinateur ou à deux joueurs.
 
 (Pour le moment, uniquement le mode solo)
+
+<!-- // Vérifie les lignes
+for (int i = 0; i < 3; i++) {
+if (tab[i][0] == tab[i][1] && tab[i][1] == tab[i][2] && tab[i][0] != ' ') {
+return tab[i][0];
+}
+}
+
+    // Vérifie les colonnes
+    for (int i = 0; i < 3; i++) {
+        if (tab[0][i] == tab[1][i] && tab[1][i] == tab[2][i] && tab[0][i] != ' ') {
+            return tab[0][i];
+        }
+    }
+
+    // Vérifie les diagonales
+    if (tab[0][0] == tab[1][1] && tab[1][1] == tab[2][2] && tab[0][0] != ' ') {
+        return tab[0][0];
+    }
+
+    if (tab[0][2] == tab[1][1] && tab[1][1] == tab[2][0] && tab[0][2] != ' ') {
+        return tab[0][2];
+    } -->
+
+quand on fait entrer ça met le x ou o dans l'emplace du tableau ou on se situe
--- main.cpp
+++ main.cpp
@@ -1,7 +1,109 @@
-#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void draw(char (*array)[3])
+{
+  // inconvéient: pas dynamique
+  cout << " " << array[0][0] << " | " << array[0][1] << " | " << array[0][2] << endl;
+  cout << "-----------" << endl;
+  cout << " " << array[1][0] << " | " << array[1][1] << " | " << array[1][2] << endl;
+  cout << "-----------" << endl;
+  cout << " " << array[2][0] << " | " << array[2][1] << " | " << array[2][2] << endl;
+}
+
+bool up(int x, int y, char (*array)[3])
+{
+  if (x > 0 && array[x - 1][y] == ' ')
+  {
+    array[x - 1][y] = '.';
+    array[x][y] = ' ';
+    return true;
+  }
+  return false;
+}
+
+bool down(int x, int y, char (*array)[3])
+{
+  if (x < 2 && array[x + 1][y] == ' ')
+  {
+    array[x + 1][y] = '.';
+    array[x][y] = ' ';
+    return true;
+  }
+  return false;
+}
+
+bool left(int x, int y, char (*array)[3])
+{
+  if (y > 0 && array[x][y - 1] == ' ')
+  {
+    array[x][y - 1] = '.';
+    array[x][y] = ' ';
+    return true;
+  }
+  return false;
+}
+
+bool right(int x, int y, char (*array)[3])
+{
+  if (y < 2 && array[x][y + 1] == ' ')
+  {
+    array[x][y + 1] = '.';
+    array[x][y] = ' ';
+    return true;
+  }
+  return false;
+}
+
+void playerTurn(char (*array)[3])
+{
+  bool done = false;
+  char input;
+  int x = 1;
+  int y = 1;
+  while (!done)
+  {
+    input = _getch();
+    switch (input)
+    {
+    case 'z':
+      if (up(x, y, array))
+      {
+        x--;
+      }
+      break;
+    case 's':
+      if (down(x, y, array))
+      {
+        x++;
+      }
+      break;
+    case 'q':
+      if (left(x, y, array))
+      {
+        y--;
+      }
+      break;
+    case 'd':
+      if (right(x, y, array))
+      {
+        y++;
+      }
+      break;
+    default:
+      cout << "Invalid command";
+      break;
+    }
+    draw(array);
+  }
+}
 
 int main()
 {
-  printf("Hello World!");
-  return 0;
+  bool end = false;
+  char array[3][3] = {{'X', ' ', ' '}, {' ', ' ', 'O'}, {' ', ' ', ' '}};
+  draw(array);
+  playerTurn(array);
 }
